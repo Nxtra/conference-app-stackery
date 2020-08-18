f7a880953c0426807d062442a49aba96601ddd67
Nxtra <nickvanhoof1@gmail.com>
2020-08-18 17:26:14 +0200

    add lambda powertools

--- src/CreateNewLearningFunction/handler.py
+++ src/CreateNewLearningFunction/handler.py
@@ -5,22 +5,33 @@ import uuid
 
 import boto3
 
+from aws_lambda_powertools import Logger
+# POWERTOOLS_SERVICE_NAME defined
+logger = Logger()
+
 dynamodb = boto3.client('dynamodb')
 learnings_table_name = os.environ['TABLE_NAME']
 
-
+@logger.inject_lambda_context
 def handle(event, context):
     # Log the event argument for debugging and for use in local development.
+    logger.info("received new event")
+
+
     learning = create_learning(json.loads(event['body']))
-    print(f'learning that was created: {json.dumps(learning)}')
+    logger.info(f'learning that was created: {json.dumps(learning)}')
 
     return {
         'learning': learning
     }
 
 
+def validate_learning_info(learning_info):
+    logger.info('validated')
+
+
 def create_learning(learning_info):
-    print(json.dumps(learning_info))
+    logger.info(json.dumps(learning_info))
     current_time_in_millis = int(round(time.time() * 1000))
     learning = {
         'creator': learning_info.get('creator'),
@@ -29,14 +40,14 @@ def create_learning(learning_info):
         'learning': learning_info.get('learning'),
         'creationTimeStamp': str(current_time_in_millis)
     }
-    print(f'will create learning ${learning}')
+    logger.info(f'will create learning ${learning}')
     save_learning(learning)
     return learning
 
 
 def save_learning(learning):
     learning_id = str(uuid.uuid4())
-    print(learning)
+    logger.info(learning)
     item = {
         'id': {'S': learning_id},
         'creator': {'S': learning['creator']},
@@ -45,9 +56,9 @@ def save_learning(learning):
         'learning': {'S': str(learning['learning'])},
         'creationTimeStamp': {'S': learning['creationTimeStamp']}
     }
-    print(item)
+    logger.info(item)
     response = dynamodb.put_item(
         TableName=learnings_table_name,
         Item=item
     )
-    print("UPLOADED ITEM")
+    logger.info("UPLOADED ITEM")
