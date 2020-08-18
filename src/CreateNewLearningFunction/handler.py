09953f20478954a398c2afc18756c083eb15566b
Nxtra <nickvanhoof1@gmail.com>
2020-08-18 11:53:33 +0200

    no more upload

--- src/CreateNewLearningFunction/handler.py
+++ src/CreateNewLearningFunction/handler.py
@@ -10,24 +10,24 @@ def handle(event, context):
     return {}
 
 
-def upload():
-    with open('data.json', 'r') as datafile:
-        records = json.load(datafile)
-    for song in records:
-        print(song)
-        item = {
-            'artist':{'S':song['artist']},
-            'song':{'S':song['song']},
-            'id':{'S': song['id']},
-            'priceUsdCents':{'S': str(song['priceUsdCents'])},
-            'publisher':{'S': song['publisher']}
-        }
-        print(item)
-        response = dynamodb.put_item(
-            TableName='basicSongsTable',
-            Item=item
-        )
-        print("UPLOADING ITEM")
-        print(response)
-
-upload()
+# def upload():
+#     with open('data.json', 'r') as datafile:
+#         records = json.load(datafile)
+#     for song in records:
+#         print(song)
+#         item = {
+#             'artist':{'S':song['artist']},
+#             'song':{'S':song['song']},
+#             'id':{'S': song['id']},
+#             'priceUsdCents':{'S': str(song['priceUsdCents'])},
+#             'publisher':{'S': song['publisher']}
+#         }
+#         print(item)
+#         response = dynamodb.put_item(
+#             TableName='basicSongsTable',
+#             Item=item
+#         )
+#         print("UPLOADING ITEM")
+#         print(response)
+#
+# upload()
